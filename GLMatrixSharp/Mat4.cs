cbe0ab4b9f53305b40e6e1c2be07fb4e5c8abfe3
DominikNITA <36695471+DominikNITA@users.noreply.github.com>
2020-12-18 14:07:08 +0100

    GLMatrixSharp: Translate method added

--- GLMatrixSharp/Mat4.cs
+++ GLMatrixSharp/Mat4.cs
@@ -28,5 +28,34 @@ namespace GLMatrixSharp
             res[15] = 1;
             return res;
         }
+
+        public static float[] Translate(float[] a, float[] v)
+        {
+            var res = new float[16];
+
+            if (a.Length < 16 || v.Length < 3) throw new ArgumentException();
+
+            float x = v[0], y = v[1], z = v[2];
+
+            res[0] = a[0];
+            res[1] = a[1];
+            res[2] = a[2];
+            res[3] = a[3];
+            res[4] = a[4];
+            res[5] = a[5];
+            res[6] = a[6];
+            res[7] = a[7];
+            res[8] = a[8];
+            res[9] = a[9];
+            res[10] = a[10];
+            res[11] = a[11];
+
+            res[12] = a[0] * x + a[4] * y + a[8] * z + a[12];
+            res[13] = a[1] * x + a[5] * y + a[9] * z + a[13];
+            res[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
+            res[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
+
+            return res;
+        }
     }
 }
